60010dafb84a00ed657c677e52e9eaa677ce82ec
eric <eegli@users.noreply.github.com>
2020-06-07 12:15:35 +0200

    renaming

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { request } from '../api/graphql';
 import { IMGS_HOME } from '../api/queries';
 import LayouContainer from '@/shared/layout/layout.container';
 import { DescriptionContainer, Sh1, Sh3, Sp } from '@/shared/headings.styles';
 import { Svideo } from '@/shared/elements.styles';
+import { FaAngleDown } from 'react-icons/fa';
 
 import {
-  IndexVideoContainer,
-  IndexContentContainer,
-  StyledArrowDown,
+  LandingVideoContainer,
+  LandingContentContainer,
+  ArrowDownContainer,
 } from './index.styles';
 
 const IndexPage = ({ images }) => {
+  const contentRef = useRef(null);
+  const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop);
   return (
     <LayouContainer>
-      <IndexVideoContainer>
+      <LandingVideoContainer>
         <Svideo autoPlay loop muted>
           <source src='/static/vid/home_teaser.mp4' type='video/mp4' />
         </Svideo>
-        <StyledArrowDown />
-      </IndexVideoContainer>
+        <ArrowDownContainer onClick={() => scrollToRef(contentRef)}>
+          <FaAngleDown />
+        </ArrowDownContainer>
+      </LandingVideoContainer>
 
-      <IndexContentContainer>
+      <LandingContentContainer ref={contentRef}>
         <Sh1 landing>| eric.egli |</Sh1>
 
         <DescriptionContainer>
@@ -32,7 +37,7 @@ const IndexPage = ({ images }) => {
         {/* {images.map(img => (
             <img key={img.url} src={img.url} />
           ))} */}
-      </IndexContentContainer>
+      </LandingContentContainer>
     </LayouContainer>
   );
 };
--- src/pages/index.styles.js
+++ src/pages/index.styles.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
-import { FaAngleDown } from 'react-icons/fa';
 
-export const IndexVideoContainer = styled.div`
+export const LandingVideoContainer = styled.div`
   position: absolute;
   top: 4.2rem;
   bottom: 0;
@@ -9,7 +8,7 @@ export const IndexVideoContainer = styled.div`
   overflow: hidden;
   z-index: -1;
 `;
-export const IndexContentContainer = styled.div`
+export const LandingContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -19,10 +18,16 @@ export const IndexContentContainer = styled.div`
   top: 100%;
 `;
 
-export const StyledArrowDown = styled(FaAngleDown)`
+export const ArrowDownContainer = styled.div`
   opacity: 0.6;
   position: absolute;
   bottom: 2rem;
   z-index: 999;
   left: 50%;
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
