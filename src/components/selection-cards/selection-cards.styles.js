0645c107609149e2f32b03ad1cab01a2959e8fb3
eric <eegli@users.noreply.github.com>
2020-06-11 19:05:23 +0200

    wip

--- src/components/selection-cards/selection-cards.styles.js
+++ src/components/selection-cards/selection-cards.styles.js
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 
 export const SelectionCardContainer = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   overflow-x: scroll;
+  width: 100%;
 `;
 
 export const CardContainer = styled.div`
@@ -11,13 +12,10 @@ export const CardContainer = styled.div`
   padding: 1.2rem;
   margin: 0 0.5rem 0.5rem 0;
   border-radius: 0.6rem;
-  &:first-child {
-    margin-left: 2rem;
-  }
   &:active,
   &:hover {
     background-color: ${({ theme }) => theme.textColor};
-    color: ${({ theme }) => theme.baseBgraised};
+    color: ${({ theme }) => theme.textShadowColor};
     cursor: pointer;
   }
 `;
