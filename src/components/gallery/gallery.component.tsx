3bc7d68c0c5c28014dd1dc49ba2eb2fd9a04c308
eric <eegli@users.noreply.github.com>
2020-06-11 10:56:47 +0200

    image gallery logic

--- src/components/gallery/gallery.component.tsx
+++ src/components/gallery/gallery.component.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
-import { GalleryContainer, GalleryImg } from './gallery.styles';
+import { GalleryContainer, Img, ImageRow, ImageCol } from './gallery.styles';
 import { IndexProps } from '../../pages/index';
 
 const Gallery: React.FC<IndexProps> = ({ images }) => {
+  // How many colums
+  const columsAmount = 3;
+  // How many images
+  const imgLen = images.length;
+  // Images per colum
+  const imgPerColum = Math.ceil(imgLen / columsAmount);
+  console.log(imgPerColum);
+
+  let collArrays: any[] = [];
+  // Create an array for each column
+  for (let col = 0; col < imgPerColum + 1; col++) {
+    collArrays.push(
+      images.slice(imgPerColum * col, imgPerColum * col + imgPerColum)
+    );
+  }
+
+  console.log(collArrays);
+
   return (
     <GalleryContainer>
-      {images.map(e => (
-        <GalleryImg key={e.url} src={e.url}></GalleryImg>
-      ))}
+      <ImageRow>
+        {collArrays.map(imgCol => (
+          <ImageCol key={Math.random()}>
+            {imgCol.map(img => (
+              <Img key={img.url} src={img.url} />
+            ))}
+          </ImageCol>
+        ))}
+      </ImageRow>
     </GalleryContainer>
   );
 };
--- src/components/gallery/gallery.styles.js
+++ src/components/gallery/gallery.styles.js
@@ -1,14 +1,38 @@
 import styled from 'styled-components';
 
-export const GalleryContainer = styled.div`
+export const GalleryContainer = styled.div``;
+
+export const ImageRow = styled.ul`
+  display: -ms-flexbox; /* IE10 */
   display: flex;
+  -ms-flex-wrap: wrap; /* IE10 */
   flex-wrap: wrap;
-  margin: 1rem 0;
-  justify-content: center;
+  padding: 0 4px;
 `;
 
-export const GalleryImg = styled.img`
-  max-width: 100%;
-  max-height: 100%;
-  margin: 1rem 0;
+export const ImageCol = styled.li`
+  -ms-flex: 25%; /* IE10 */
+  flex: 25%;
+  max-width: 25%;
+  padding: 0 4px;
+  @media screen and (max-width: 800px) {
+    & {
+      -ms-flex: 50%;
+      flex: 50%;
+      max-width: 50%;
+    }
+  }
+  @media screen and (max-width: 600px) {
+    & {
+      -ms-flex: 100%;
+      flex: 100%;
+      max-width: 100%;
+    }
+  }
+`;
+
+export const Img = styled.img`
+  margin-top: 8px;
+  vertical-align: middle;
+  width: 100%;
 `;
