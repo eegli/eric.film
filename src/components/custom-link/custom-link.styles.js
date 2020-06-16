567cd1bfd47700c15b6aad67a202ffc6fbb72806
eric <eegli@users.noreply.github.com>
2020-06-16 20:34:40 +0200

    wip

--- src/components/custom-link/custom-link.styles.js
+++ src/components/custom-link/custom-link.styles.js
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 export const CustomLinkText = styled.a`
   color: ${({ theme }) => theme.textLinkColor};
   background-color: ${({ theme }) => theme.baseBgraised};
-  font-size: 1rem;
+  font-size: ${({ theme }) => theme.fontsizes.baseFontSize};
   text-decoration: none;
   border-top: 0.1rem solid white;
   border-left: 0.1rem solid white;
   border-color: ${({ theme }) => theme.textShadowColor};
-  padding: 0.2rem 0.5rem 0.5rem 0.5rem;
+  padding: 0.5rem 0.5rem 0.5rem 0.5rem;
   border-radius: 0.3rem;
   letter-spacing: 0.4rem;
   &:hover {
