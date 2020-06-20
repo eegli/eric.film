37e9409e0eeae00b43a7882fe5864abf2f56bb78
eric <eegli@users.noreply.github.com>
2020-06-21 01:19:42 +0200

    SAFARI FIX

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -15,20 +15,20 @@ const IndexPage: React.FC = () => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
-  const apolloClient = initializeApollo();
+// export const getStaticProps: GetStaticProps = async () => {
+//   const apolloClient = initializeApollo();
 
-  await apolloClient.query({
-    query: ALL_BLOGPOSTS_PREVIEW,
-    variables: allBlogPostsVars(),
-  });
+//   await apolloClient.query({
+//     query: ALL_BLOGPOSTS_PREVIEW,
+//     variables: allBlogPostsVars(),
+//   });
 
-  return {
-    props: {
-      initialApolloState: apolloClient.cache.extract(),
-    },
-    // unstable_revalidate: 1,
-  };
-};
+//   return {
+//     props: {
+//       initialApolloState: apolloClient.cache.extract(),
+//     },
+//     // unstable_revalidate: 1,
+//   };
+// };
 
 export default IndexPage;
