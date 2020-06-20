f31258b43d5ed058aef7b874c3c21e4807142580
eric <eegli@users.noreply.github.com>
2020-06-21 00:50:02 +0200

    safari...

--- next.config.js
+++ next.config.js
@@ -4,14 +4,4 @@ module.exports = {
     autoPrerender: true,
   },
   distDir: 'build',
-  // https://github.com/vercel/next.js/issues/9851
-  webpack: config => {
-    const alias = { ...config.resolve.alias };
-    delete alias.url; // alias to native-url
-    config.resolve = {
-      ...config.resolve,
-      alias,
-    };
-    return config;
-  },
 };
