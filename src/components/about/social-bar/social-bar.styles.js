1ebaa28f00f92649920047d692fbf36a839071b1
eric <eegli@users.noreply.github.com>
2020-07-11 12:43:28 +0200

    social icons adjustments

--- src/components/about/social-bar/social-bar.styles.js
+++ src/components/about/social-bar/social-bar.styles.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
 `;
 
 export const IconContainer = styled.a`
-  padding: 0 0.4rem;
+  margin: 0 0.5rem;
   font-size: ${({ theme }) => theme.fontsizes.baseIconFontSize};
   color: ${({ theme }) => theme.colors.textSecondaryColor};
 `;
