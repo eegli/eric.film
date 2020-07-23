8118bd259986243c08d6d9ec95c97c17a7116f19
eric <eegli@users.noreply.github.com>
2020-07-23 10:53:10 +0200

    updated tests

--- src/components/about/about.test.jsx
+++ src/components/about/about.test.jsx
@@ -1,9 +1,15 @@
 import { shallow } from '@/src/tests/testUtils';
 import About from './about.component';
+import Cards from './cards/card.component';
+import Resume from './resume/resume.component';
 
 describe('About', () => {
-  it('renders shallow wrapper', () => {
-    const shallowWrapper = shallow(<About />);
-    expect(shallowWrapper).toMatchSnapshot();
+  const wrapper = shallow(<About />);
+
+  it('shows resume component', () => {
+    expect(wrapper.find(Cards)).toHaveLength(1);
+  });
+  it('shows resume component', () => {
+    expect(wrapper.find(Resume)).toHaveLength(1);
   });
 });
