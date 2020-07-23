e951875463eb27917bf94d9c4ab4c4de665fbfa5
eric <eegli@users.noreply.github.com>
2020-07-23 18:39:06 +0200

    updated snapshots

--- src/components/about/resume/resume.test.tsx
+++ src/components/about/resume/resume.test.tsx
@@ -1,20 +1,9 @@
-import { mountWithTheme, shallow } from '@/src/tests/testUtils';
-import SmoothCollapse from 'react-smooth-collapse';
+import { shallow } from '@/src/tests/testUtils';
 import Resume from './resume.component';
-import { ToggleContainer } from './resume.styles';
 
 describe('About', () => {
-  const mountedWrapper = mountWithTheme(<Resume />);
-  const shallowWrapper = shallow(<Resume />);
-
-  it('renders resume component', () => {
+  it('renders about component', () => {
+    const shallowWrapper = shallow(<Resume />);
     expect(shallowWrapper).toMatchSnapshot();
-  });
-
-  it('toggles education div', () => {
-    const toggler = mountedWrapper.find(ToggleContainer).at(0);
-    const collapse = mountedWrapper.find(SmoothCollapse).at(0);
-
-    expect(collapse.props().expanded).toBe(false);
   });
 });
