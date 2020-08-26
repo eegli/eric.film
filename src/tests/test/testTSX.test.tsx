626bcfd8ffe461fb5a180c561df1458deadcb81c
eric <eegli@users.noreply.github.com>
2020-08-26 17:31:08 +0200

    schema.org improvements

--- src/tests/test/testTSX.test.tsx
+++ src/tests/test/testTSX.test.tsx
@@ -4,7 +4,6 @@ const Page: React.FC = () => <div>Test</div>;
 
 describe('Test - TSX', () => {
   it('renders test TSX component', () => {
-    const wrapper = shallow(<Page />);
     expect(shallow).toMatchSnapshot();
   });
 });
--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,20 +1,22 @@
 // https://developers.google.com/search/docs/data-types/article
 
+// There is a type definition package for schema.org here:
+// https://www.npmjs.com/package/schema-dts
+// but adding 1mb overhead for one file is not worth it.
+
 import { BlogPostContent } from '@/components/types';
 import { BLOG_PREVIEW_IMG_FALLBACK } from '@/src/config';
 
-export const makeBlogSchema = (post: BlogPostContent) => {
+export const makeBlogSchema = (post: BlogPostContent): string => {
   const image = post.previewImage
     ? post.previewImage.url
     : BLOG_PREVIEW_IMG_FALLBACK.url;
-  const schema = {
+  return JSON.stringify({
     '@context': 'http://schema.org',
     '@type': 'BlogPosting',
     datePublished: post.createdAt,
     dateModified: post.updatedAt,
     headline: post.title,
     image: [image],
-  };
-
-  return JSON.stringify(schema);
+  });
 };
