5c0ee7d678145648127fc55d1dfd1d6fa4be764a
eric <eegli@users.noreply.github.com>
2020-09-04 19:55:46 +0200

    experimental font loading

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,6 +38,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en' prefix='og: https://ogp.me/ns#'>
         <Head>
+          <link
+            href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
+            rel='stylesheet'
+          />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
