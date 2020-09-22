bb0f4edddef7acc4b2b7d743fd4886761a25a45e
eric <eegli@users.noreply.github.com>
2020-09-22 11:22:35 +0200

    update tests

--- src/components/markdown/markdown.test.tsx
+++ src/components/markdown/markdown.test.tsx
@@ -4,6 +4,13 @@ import Markdown from './markdown.component';
 
 describe('Markdown', () => {
   it('renders markdown content', () => {
+    const intersectionObserverMock = () => ({
+      observe: () => null,
+    });
+    window.IntersectionObserver = jest
+      .fn()
+      .mockImplementation(intersectionObserverMock);
+
     const wrapper = mountWithTheme(<Markdown content={blogpost.content} />);
     expect(wrapper).toMatchSnapshot();
   });
