5ba342e6bfbbd6b7404c3c7c267798ea54dac3c3
Eric Egli <43848365+eegli@users.noreply.github.com>
2020-12-29 10:45:10 +0100

    comment out disqus

--- src/components/blog/blog-container.component.tsx
+++ src/components/blog/blog-container.component.tsx
@@ -3,18 +3,17 @@ import ErrorMessage from '@/components/error-message/error-message.component';
 import { useBlogpostQuery } from '@/components/types';
 import { getElementFromArray } from '@/src/utils/array';
 import { makeBlogpostSchema } from '@/src/utils/schema';
-import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { CommentInfo, Container } from './blog-container.styles';
+import { Container } from './blog-container.styles';
 import BlogContent from './content/blog-content.component';
 
-const CustomDisqus = dynamic(
+/* const CustomDisqus = dynamic(
   () => import('@/components/custom-disqus/custom-disqus.component'),
   {
     ssr: false,
   },
-);
+); */
 
 const BlogContainer: React.FC = () => {
   const router = useRouter();
@@ -54,11 +53,11 @@ const BlogContainer: React.FC = () => {
         <Container>
           <BlogContent {...post} />
           <div style={{ padding: '0 1rem' }}>
-            <CommentInfo>
+            {/*             <CommentInfo>
               Note: In order to comment as a guest, click the "Name" field and
               then select "I'd rather post as a guest" from the options.
-            </CommentInfo>
-            <CustomDisqus id={post.id} title={post.title} slug={post.slug} />
+            </CommentInfo> */}
+            {/*  <CustomDisqus id={post.id} title={post.title} slug={post.slug} /> */}
           </div>
         </Container>
       </>
